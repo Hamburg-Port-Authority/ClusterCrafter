36b78325897d28d5776369e0802b80ee2d405740
deB4SH <git@b4sh.de>
2023-11-09 13:20:13 +0100

    space logs a bit

--- build-and-test.sh
+++ build-and-test.sh
@@ -14,5 +14,13 @@ docker run \
     -v ./test-area/coreService/config:/data \
     -v ./test-area/coreService/out:/output \
     -i -t $(echo "$REGISTRY/cluster-crafter:$TAG")
-
+echo " "
+echo "== checking for diffs now"
+echo " "
 diff -bur ./test-area/coreService/out ./test-area/coreService/expected    
+retVal=$?
+if [ $retVal -ne 0 ]; then
+    echo " "
+    echo "Seems that there was a diff between generated out(./test-area/coreService/out) and expected(./test-area/coreService/expected)."
+    exit 0
+fi
