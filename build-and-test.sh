0d33b5af27fa8e4026fc44aa0a11e0ac525744e8
deB4SH <git@b4sh.de>
2023-11-14 14:34:14 +0100

    add: test script block

--- build-and-test.sh
+++ build-and-test.sh
@@ -3,24 +3,49 @@ source build.sh
 
 # RUN TEST #1 (coreService)
 
+# echo "clean off old test data."
+# find ./test-area/coreService/out -type f -not -name '.gitignore' -delete
+# echo "done cleaning off old test data."
+
+# docker run \
+#     --env templatedirectory=/template/overlay/coreService \
+#     --env schemafilename=coreServiceSchema.json \
+#     --env configfile=/data/coreServiceConfig.yaml \
+#     -v ./test-area/coreService/config:/data \
+#     -v ./test-area/coreService/out:/output \
+#     -i -t $(echo "$REGISTRY/cluster-crafter:$TAG")
+# echo " "
+# echo "== checking for diffs now"
+# echo " "
+# diff -bur ./test-area/coreService/out ./test-area/coreService/expected    
+# retVal=$?
+# if [ $retVal -ne 0 ]; then
+#     echo " "
+#     echo "Seems that there was a diff between generated out(./test-area/coreService/out) and expected(./test-area/coreService/expected)."
+#     exit 0
+# fi
+
+
+# RUN TEST #2 (observability)
+
 echo "clean off old test data."
-find ./test-area/coreService/out -type f -not -name '.gitignore' -delete
+find ./test-area/observability/out -type f -not -name '.gitignore' -delete
 echo "done cleaning off old test data."
 
 docker run \
-    --env templatedirectory=/template/overlay/coreService \
-    --env schemafilename=coreServiceSchema.json \
-    --env configfile=/data/coreServiceConfig.yaml \
-    -v ./test-area/coreService/config:/data \
-    -v ./test-area/coreService/out:/output \
+    --env templatedirectory=/template/observability \
+    --env schemafilename=observability.json \
+    --env configfile=/data/config.yaml \
+    -v ./test-area/observability/config:/data \
+    -v ./test-area/observability/out:/output \
     -i -t $(echo "$REGISTRY/cluster-crafter:$TAG")
 echo " "
 echo "== checking for diffs now"
 echo " "
-diff -bur ./test-area/coreService/out ./test-area/coreService/expected    
+diff -bur ./test-area/observability/out ./test-area/observability/expected    
 retVal=$?
 if [ $retVal -ne 0 ]; then
     echo " "
-    echo "Seems that there was a diff between generated out(./test-area/coreService/out) and expected(./test-area/coreService/expected)."
+    echo "Seems that there was a diff between generated out(./test-area/observability/out) and expected(./test-area/observability/expected)."
     exit 0
 fi
